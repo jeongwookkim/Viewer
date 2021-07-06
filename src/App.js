86d6f554d8c33db665fc42a4c80e6a6e75870a97
jeongwookkim <mybiggold@naver.com>
2021-07-06 18:06:45 +0900

    [feat] 유튜브 스트리밍 영상, 유튜브 영상 플레이어, src 내에 저장 동영상 불러오기 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Home, StudyLesson, ReadContent, Epub } from './Page';
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
           <div className="Nav">
-                <Link to="/">홈</Link>
-                <Link to="/StudyLesson">StudyLesson</Link>
-                <Link to="/ReadContent">동영상 강의 보기</Link>
+                <Link className="Link" to="/">홈</Link>
+                <Link className="Link" to="/StudyLesson">영상 보기</Link>
+                <Link className="Link" to="/ReadContent">Epub 보기</Link>
                 
           </div>
           <div className="Main">
--- src/Page/StudyLesson.js
+++ src/Page/StudyLesson.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../App.css';
+
+import ReactPlayer from 'react-player'
+import videoFilePath from '../videos/video1.mp4'
+
+
 
 function repeatUI() {
     var forArray = [];
@@ -8,6 +13,7 @@ function repeatUI() {
         <div className = "Flex">
           <div>강의{i}</div>
           <button className="btn">강의 듣기</button>
+          
         </div>
         );
     }
@@ -17,6 +23,17 @@ function repeatUI() {
 const StudyLesson = () => {
     return(
         <div>
+            <h3>유튜브 영상 스트리밍</h3>
+            <ReactPlayer
+            url='https://www.youtube.com/watch?v=hJxoXR7AOr4' playing controls/>
+            <hr></hr>
+            <h3>유튜브 영상 플레이어</h3>
+            <ReactPlayer
+            url='https://www.youtube.com/watch?v=ERGth50aA-M' playing controls/>
+            <hr></hr>
+            <h3>src 내에 저장 동영상 불러오기</h3>
+            <ReactPlayer url={videoFilePath} width="600px" height="300px" controls={true} />
+            <hr></hr>
             {repeatUI()}
         </div>
     )
