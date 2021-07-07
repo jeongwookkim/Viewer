ade28a2dc0e281a7065471fca52ccae2aa7f1503
jeongwookkim <mybiggold@naver.com>
2021-07-07 16:11:03 +0900

    [feat] hls 동영상 재생, hls public내에 파일 재생 기능 추가

--- src/Page/StudyLesson.js
+++ src/Page/StudyLesson.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import '../App.css';
 
-import ReactPlayer from 'react-player'
-import videoFilePath from '../videos/video1.mp4'
+import ReactPlayer from 'react-player';
+import videoFilePath from '../videos/video1.mp4';
+import ReactHlsPlayer from 'react-hls-player';
 
 
 
@@ -10,7 +11,7 @@ function repeatUI() {
     var forArray = [];
     for (let i = 1; i < 10; i++) {
         forArray.push(
-        <div className = "Flex">
+        <div>
           <div>강의{i}</div>
           <button className="btn">강의 듣기</button>
           
@@ -23,16 +24,33 @@ function repeatUI() {
 const StudyLesson = () => {
     return(
         <div>
-            <h3>유튜브 영상 스트리밍</h3>
-            <ReactPlayer
-            url='https://www.youtube.com/watch?v=hJxoXR7AOr4' playing controls/>
+            <div className="Flex">
+                <div>
+                <h3>유튜브 영상 스트리밍</h3>
+                <ReactPlayer
+                url='https://www.youtube.com/watch?v=hJxoXR7AOr4' playing controls/>
+                </div>
+                <div>
+                <h3>유튜브 영상 플레이어</h3>
+                <ReactPlayer
+                url='https://www.youtube.com/watch?v=ERGth50aA-M' playing controls/>
+                </div>
+            </div>
             <hr></hr>
-            <h3>유튜브 영상 플레이어</h3>
-            <ReactPlayer
-            url='https://www.youtube.com/watch?v=ERGth50aA-M' playing controls/>
-            <hr></hr>
-            <h3>src 내에 저장 동영상 불러오기</h3>
-            <ReactPlayer url={videoFilePath} width="600px" height="300px" controls={true} />
+            <div  className="Flex">
+                <div>
+                    <h3>src 내에 저장 동영상 불러오기</h3>
+                    <ReactPlayer url={videoFilePath} width="600px" height="300px" controls={true} />
+                </div>
+                <div>
+                    <h3>HLS 동영상 재생 (url)</h3>
+                    <ReactHlsPlayer src="https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8" width="600px" height="300px" controls={true} />
+                </div>
+                <div>
+                    <h3>HLS 동영상 재생 (public내에  파일)</h3>
+                    <ReactHlsPlayer src={window.location.origin + '/hlsVideos/playlist.m3u8'} width="600px" height="300px" controls={true} />
+                </div>
+            </div>
             <hr></hr>
             {repeatUI()}
         </div>
